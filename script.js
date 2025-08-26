e9624126255585a02031e4176ab0605204eefe30
Paul Orschau <paul.orschau@icloud.com>
2025-08-26 23:00:09 +0200

    Filter Funktion korrigiert

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ document.getElementById('year').textContent = new Date().getFullYear();
 
 fetch('plants.json', { cache: 'no-store' })
 .then(r => r.json())
-.then(data => render(data))
+.then(data => {
+  setCategoryOptions(data);
+  render(data);
+})
 .catch(err => {
-list.innerHTML = '<li>Fehler beim Laden der Stocklist.</li>';
-console.error(err);
+  list.innerHTML = '<li>Fehler beim Laden der Stocklist.</li>';
+  console.error(err);
 });
 
 
 function render(plants) {
 list.innerHTML = plants.map(p => `<li>${escapeHTML(p.name)}</li>`).join('');
+}
+
+
+// Dynamisch die Kategorie-Optionen aus den Daten generieren
+function setCategoryOptions(plants) {
+  const typeFilter = document.getElementById('typeFilter');
+  const categories = Array.from(new Set(plants.map(p => p.category)));
+  typeFilter.innerHTML = '<option value="">Typ: Alle</option>' +
+    categories.map(cat => `<option value="${cat}">${cat.charAt(0).toUpperCase() + cat.slice(1)}</option>`).join('');
 }
 
 
